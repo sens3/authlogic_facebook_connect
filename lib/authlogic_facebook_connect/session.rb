fc0b6867fcf574c7f110dc5b8975f10467b60513
smn <smn@sens3.com>
2010-03-10 17:08:57 -0500

    avoiding errors when running model specs (authlogic mock controller doesnt have know set_facebook_session)

--- lib/authlogic_facebook_connect/session.rb
+++ lib/authlogic_facebook_connect/session.rb
@@ -129,8 +129,10 @@ module AuthlogicFacebookConnect
       end
 
       def authenticating_with_facebook_connect?
-        controller.set_facebook_session
-        attempted_record.nil? && errors.empty? && controller.facebook_session
+        if controller.respond_to?(:set_facebook_session) 
+          controller.set_facebook_session
+          attempted_record.nil? && errors.empty? && controller.facebook_session
+        end
       end
 
       private
