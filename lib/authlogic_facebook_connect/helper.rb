8a921860e2dd4f3ae2a57e6db735abea48109611
smn <smn@sens3.com>
2010-03-16 17:04:27 -0400

    using alternative onlogin option instead of before_submit

--- lib/authlogic_facebook_connect/helper.rb
+++ lib/authlogic_facebook_connect/helper.rb
@@ -13,17 +13,18 @@ module AuthlogicFacebookConnect
         js_selector = "jQuery('#connect_to_facebook_form')"
       end
       
+      onlogin = options[:onlogin] || "connect_to_facebook()"
+      
       output = "<form id='connect_to_facebook_form' method='post' action='/#{options[:controller]}'>\n"
       output << "<input type='hidden' name='authenticity_token' value='#{form_authenticity_token}'/>\n"
       output << "</form>\n"
       output << "<script type='text/javascript' charset='utf-8'>\n"
       output << " function connect_to_facebook() {\n"
-      output << "   #{options[:before_submit]};\n" if options[:before_submit].present?
       output << "   #{js_selector}.submit();\n"
       output << " }\n"
       output << "</script>\n"
       options.delete(:controller)
-      output << fb_login_button("connect_to_facebook()", options)
+      output << fb_login_button(onlogin, options)
       output
     end
   end
